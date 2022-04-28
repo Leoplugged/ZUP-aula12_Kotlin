de75463fb1e83e82d932047bb3228cf3b2bb83aa
LeoPlugged <leandro.miguel@outlook.com>
2022-04-27 21:58:01 -0300

    Acrescentar novo looping de repetição no exercício 4 e consertar a biblioteca para rodar o readln()

--- src/Aula12ex04.kt
+++ src/Aula12ex04.kt
@@ -6,10 +6,11 @@ b. se a temperatura estiver entre 19 e 23 graus, o clima é agradável;
 c. se a temperatura estiver entre 24 e 35 graus, o clima é quente;
 d. se a temperatura estiver acima de 35 graus, o clima é muito quente.*/
 fun main() {
-    realFeel()
+    realFeelIf()
+    realFeelWhen()
 }
 
-fun realFeel() {
+fun realFeelIf() {
     print("Qual a temperatura em graus Célsius você deseja saber para verificar o tipo do clima? ")
     val temperatura = readln().toDouble()
     if(temperatura <= 18.9){
@@ -22,3 +23,23 @@ fun realFeel() {
         println("O clima é muito quente.")
     }
 }
+
+fun realFeelWhen(){
+    print("Qual a temperatura em graus Célsius você deseja saber para verificar o tipo do clima? ")
+    val temperatura = readln().toDouble()
+
+    when{
+        temperatura < 18 -> println("o clima é frio.")
+        temperatura < 23 -> println("o clima é gradável.")
+        temperatura < 35 -> println("o clima é quente.")
+        temperatura >= 35 -> println("o clima é muito quente.")
+        else -> println("Temperatura fora do padrão.")
+    }/*
+    when(temperatura){
+        in 0 .. 18 -> println("o clima é frio.")
+        in 18..23 -> println("o clima é gradável.")
+        in 23..35 -> println("o clima é quente.")
+       in temperatura > 35 -> println("o clima é muito quente.")
+       else -> println("Temperatura fora do padrão.")
+    }*/
+ }
